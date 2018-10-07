8ce0624b33e11696864a62915c076b4047ecbc91
Enrico <enricoseo@gmail.com>
2018-10-08 00:27:45 +0300

    wrw

--- rank/accounts/tests.py
+++ rank/accounts/tests.py
@@ -36,7 +36,7 @@ class TestUserManagement(TestCase):
 
         # логинимся
         response = self.client.get(reverse('accounts:sign-in'))
-        print(response.context)
+        print(response.context_data)
         # self.assertFalse(response.context['user'].is_anonymous)
         self.assertEqual(response.context['user'], self.superuser)
 
